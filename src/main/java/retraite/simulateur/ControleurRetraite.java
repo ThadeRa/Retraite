3212041f3c4ba8605542c47afe44756db210a436
ThadeRa <96497635+ThadeRa@users.noreply.github.com>
2024-11-17 23:01:19 +0100

    Update ControleurRetraite.java

--- src/main/java/retraite/simulateur/ControleurRetraite.java
+++ src/main/java/retraite/simulateur/ControleurRetraite.java
@@ -21,4 +21,10 @@ public class ControleurRetraite {
         return serviceRetraite.calculerEpargneRetraite(adherent);
     }
 
+@PostMapping("/example")
+public ResponseEntity<String> handlePost() {
+    return ResponseEntity.ok("POST accepted");
+}
+
+    
 }
