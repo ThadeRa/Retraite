386e36839c898bb7d7e7830f458874bd6399f3e8
ThadeRa <96497635+ThadeRa@users.noreply.github.com>
2024-11-17 21:05:16 +0100

    Create WebConfig.java

---
+++ src/main/java/retraite/simulateur/WebConfig.java
@@ -0,0 +1,20 @@
+package com.votrepackage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // Autoriser toutes les ressources de l'application à accepter des requêtes de votre front-end
+        registry.addMapping("/**")  // Remplace "/**" par le chemin spécifique si nécessaire
+                .allowedOrigins("http://localhost:3000") // Remplacez par l'URL de votre front-end (local ou en production)
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Liste des méthodes HTTP autorisées
+                .allowedHeaders("*") // Autoriser tous les headers
+                .allowCredentials(true); // Autoriser l'envoi de cookies si nécessaire
+    }
+}
